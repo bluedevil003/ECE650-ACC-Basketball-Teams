09ee6ca32052994b87e59c8bf2abda153522094f
kw300 <kw300@duke.edu>
2020-03-14 19:13:20 -0400

    extra credit start

---
+++ extra_credit/orm/query.py
@@ -0,0 +1,15 @@
+from ncaa.models import Player, Team, State, Color
+
+def query1(use_mpg,  min_mpg,  max_mpg, use_ppg,  min_ppg,  max_ppg, use_rpg,  min_rpg,  max_rpg, use_apg,  min_apg,  max_apg, use_spg,  min_spg,  max_spg, use_bpg,  min_bpg,  max_bpg):
+
+
+def query2(team_color):
+
+
+def query3(team_name):
+
+
+def query4(team_state, team_color):
+
+
+def query5(num_wins):
