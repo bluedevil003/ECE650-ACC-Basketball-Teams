f22657a6d5ff1a5878774f9b13d548307e18f60d
kw300 <kw300@duke.edu>
2020-03-15 16:54:56 -0400

    final

--- extra_credit/orm/acc/models.py
+++ extra_credit/orm/acc/models.py
@@ -1,6 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class State(models.Model):
+    state_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+
+class Color(models.Model):
+    color_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+
+class Team(models.Model):
+    team_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    state_id = models.ForeignKey(State, on_delete=models.CASCADE)
+    color_id = models.ForeignKey(Color, on_delete=models.CASCADE)
+    wins = models.IntegerField()
+    losses = models.IntegerField()
+
 class Player(models.Model):
     player_id = models.AutoField(primary_key=True)
     team_id = models.ForeignKey(Team, on_delete=models.CASCADE)
@@ -13,19 +29,3 @@ class Player(models.Model):
     apg = models.IntegerField()
     spg = models.FloatField()
     bpg = models.FloatField()
-
-class Team(models.Model):
-    team_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100)
-    state_id = models.ForeignKey(State, on_delete=models.CASCADE)
-    color_id = models.ForeignKey(Color, on_delete=models.CASCADE)
-    wins = models.IntegerField()
-    losses = models.IntegerField()
-
-class State(models.Model):
-    state_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100)
-
-class Color(models.Model):
-    color_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100)
